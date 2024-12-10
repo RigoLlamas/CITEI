bed3664a7627f8eb43d599ea41621509ee0cecb5
RigoLlamas <rigo.llamas77@gmail.com>
2024-12-10 11:03:03 -0600

    refactor: move direct PHP automations to database methods and remove promociones.js
    - Transition PHP-based automations to automated methods within the database
    - Delete promociones.js to streamline project structure

--- promociones/promociones.js
+++
@@ -1,8 +0,0 @@
-
-
-// Ejecutar al cargar la página para aplicar el límite inicial basado en la opción preseleccionada
-document.addEventListener('DOMContentLoaded', function() {
-    var today = new Date().toISOString().split('T')[0]; // Obtiene la fecha actual en formato YYYY-MM-DD
-    document.getElementById('despliegue').setAttribute('min', today);
-    document.getElementById('expiracion').setAttribute('min', today);
-});
