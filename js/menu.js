bd86afad3b24804414eda5a23fda81acb349c978
RigoLlamas <rigo.llamas77@gmail.com>
2025-01-05 21:58:31 -0600

    chore: remove unnecessary and redundant files
    - Delete unused or irrelevant files to clean up the project directory
    - Improve project organization and maintainability

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.getElementById('menu');
-    const hamburger = document.getElementById('hamburger');
-
-    hamburger.addEventListener('click', function() {
-        // Alternamos entre mostrar y ocultar el menú
-        menu.classList.toggle('active');
-    });
-});
--- php/obtener_usuario.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$isAdmin = isset($_SESSION['id_usuario']) && $_SESSION['id_usuario'] == 1;
-header('Content-Type: application/json');
-echo json_encode($isAdmin);
-?>
