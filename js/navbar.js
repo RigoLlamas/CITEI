685c1e7bcd237f26eca43ea00b95e28d79a1b66f
RigoLlamas <rigo.llamas77@gmail.com>
2025-01-05 23:44:26 -0600

    feat: add favicon for the website
    - Include a favicon to enhance website branding and user experience
    - Ensure compatibility across different browsers and devices

--- js/navbar.js
+++ js/navbar.js
@@ -42,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+        <link rel="icon" href="img/favicon-32x32.png" type="image/png">
         <header>
             <h1 class="titulo">CITEI</h1>
         </header>
