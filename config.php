13fcfeb819c414762c1ae27b527b73842cae449b
EC2 Default User <ec2-user@ip-172-31-28-177.us-east-2.compute.internal>
2025-01-06 14:25:44 -0600

    chore: backup of the website
    - Save a complete backup of the current website version
    - Ensure a

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    Cuales son las API's usadas:
+    Distance Matrix API
+    Maps JavaScript API
+    Geocoding API
+*/
+
+return [
+    'api_keys' => [
+        'google_maps_api_key' => 'AIzaSyDTtTA9nfl0phn0i-i4VYbgRbvIB_MzaOs'
+    ],
+
+    'database' => [
+        'host' => 'citei.c34sm80cijk7.us-east-2.rds.amazonaws.com',
+        'user' => 'adminCITEI',
+        'password' => 'XjqcRn6EfvUuFIwu',
+        'dbname' => 'citei'
+    ],
+
+    'email_service' => [
+        'service_id' => 'service_xigafai',
+        'template_code' => 'template_m0cj8kg',
+        'template_message' => 'template_92c5pvi',
+        'user_id' => '7gg6Y8y5zhCzIx7qS'
+    ]
+];
+
+?>
